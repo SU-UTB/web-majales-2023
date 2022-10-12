3b0205c4b3e5672dd9eebfaf755ce2ab4339ec30
Filip Tomeš <tomes@cngroup.dk>
2022-10-12 21:01:23 +0200

    exdended tailwind colors and font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark_blu: '#3724B6',
+        light_blu: '#1A64F5',
+        pinky: '#FF3358',
+        su_orange: '#FFA723',
+        dark_blu_txt: '#391F68',
+      },
+      fontFamily: {
+        body: ['"Cabinet Grotesk"'],
+      },
+    },
   },
   plugins: [],
 };
