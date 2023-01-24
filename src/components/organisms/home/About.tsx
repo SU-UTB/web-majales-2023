a70a85a55a0c295ee62a247fa495d4dc0868f901
Filip Tomeš <tomes@cngroup.dk>
2023-01-24 15:17:58 +0200

    fixed the missed one

--- src/components/organisms/home/About.tsx
+++ src/components/organisms/home/About.tsx
@@ -1,4 +1,4 @@
-import SectionHeading from "../../atoms/section/SectionHeading";
+import SectionHeading from '../../atoms/section/SectionHeading';
 
 const About = () => {
   return (
@@ -6,7 +6,7 @@ const About = () => {
       <div className="px-8 py-8 mx-auto md:w-4/5 lg:w-3/5 md:py-16 md:px-0">
         <SectionHeading sectionHeadingText="Přijďte si poslechnout hvězdy české hudební scény. Vydejte se s námi k nebesům." />
         <p className="md:w-4/5 lg:w-3/5 xl:w-1/2">
-          <span className="txt-bold">6. května</span> se potkáme v malebném
+          <span className="txt-bold">5. května</span> se potkáme v malebném
           <span className="txt-bold"> parku před Baťovou vilou</span>. Tradičně
           se můžete těšit na mraky doprovodných eventů, připravujeme pro vás
           také soutěž kapel, nebo volbu krále a&nbsp;královny Majálesu.
