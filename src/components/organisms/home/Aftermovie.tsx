e1aa91046736dbf695bd77780c276d8c301869a0
Jan Bures <buress.jan@email.cz>
2023-04-28 09:39:24 +0200

    New aftermovie

--- src/components/organisms/home/Aftermovie.tsx
+++ src/components/organisms/home/Aftermovie.tsx
@@ -1,23 +1,18 @@
 import aftermovie from '../../../assets/video/aftermovie.mp4';
-// import aftermoviePoster from '../../../assets/video/aftermoviePoster.jpg';
 
 const Aftermovie = () => {
   return (
     <section id='aftermovie' className='relative -top-10 md:-top-16'>
       <div className='px-4 mx-auto lg:w-3/5 md:w-4/5 md:px-0 '>
         <div className='video-container'>
-          <video
-            controls
-            className='green-shadow '
-            // poster={aftermoviePoster}
-          >
+          <video controls className='green-shadow '>
             <source src={aftermovie} type='video/mp4' />
             <track kind='captions' />
             Tvúj prohlížeč nepodporuje formát videa
           </video>
         </div>
         <p className='mt-4 text-xl text-right uppercase txt-extrabold'>
-          Aftermovie 2019
+          Aftermovie 2022
         </p>
       </div>
     </section>
