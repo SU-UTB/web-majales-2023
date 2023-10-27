71bbbc5b380588feb1057d13b6201babcaa80fcb
Roman Vilčák <114882951+WilloS88@users.noreply.github.com>
2023-10-28 00:16:01 +0200

    Change the partners section  (#20)

--- src/components/organisms/partners/Partners.content.tsx
+++ src/components/organisms/partners/Partners.content.tsx
@@ -39,7 +39,6 @@ export const mainPartners: PartnerItem[] = [
     logoSrc: geniczechom,
     linkURL: 'https://www.geniczech.cz/',
   },
-
   {
     name: 'MeatFly',
     logoSrc: meatFly,
@@ -58,13 +57,11 @@ export const partners: PartnerItem[] = [
     logoSrc: cistyFestival,
     linkURL: 'https://www.cistyfestival.cz/',
   },
-
   {
     name: 'Kenneth',
     logoSrc: kenneth,
     linkURL: 'https://www.kenneth.cz/',
   },
-
   {
     name: 'Radegast',
     logoSrc: radegast,
@@ -75,43 +72,36 @@ export const partners: PartnerItem[] = [
     logoSrc: nextBike,
     linkURL: 'https://www.nextbikeczech.com/',
   },
-
   {
     name: 'Vertikon',
     logoSrc: vertikon,
     linkURL: 'https://www.vertikon.cz/',
   },
-
   {
     name: 'Kafe Na rohu',
     logoSrc: kafeNaRohu,
     linkURL: 'https://www.kafenarohu.cz/',
   },
-
   {
     name: 'Faency Fries',
     logoSrc: faencyFries,
     linkURL: 'https://www.faencyfries.cz/',
   },
-
   {
     name: 'Svet plodu',
     logoSrc: svetPlodu,
     linkURL: 'https://www.svetplodu.cz/',
   },
-
   {
     name: 'Lazo Plazo fest',
     logoSrc: lazoPlazo,
     linkURL: 'https://lazoplazofest.cz/',
   },
-
   {
     name: 'F1 fitness',
     logoSrc: f1_fitness,
     linkURL: 'http://www.fitnessf1.cz/',
   },
-
   {
     name: 'Halo bubble Tea',
     logoSrc: bubbleTea,
--- src/components/organisms/partners/Partners.tsx
+++ src/components/organisms/partners/Partners.tsx
@@ -1,7 +1,3 @@
-import { mainPartners, partners } from './Partners.content';
-import trinityBank from '../../../assets/partners/2023/trinityBank.png';
-import postaBezHranic from '../../../assets/partners/2023/mainPartners/postaBezHranic.png';
-import vest from '../../../assets/partners/2023/classicPartners/vest.png';
 import PartnerHeading from '../../atoms/section/PartnerHeading';
 
 const Partners = () => {
@@ -10,65 +6,13 @@ const Partners = () => {
       id='partneri'
       className='flex flex-col w-4/5 w-max-[1600px] m-auto my-12 xs:my-20 gap-10 xs:gap-8'
     >
-      <div className='flex flex-col w-full self-center'>
-        <PartnerHeading>Generální partner</PartnerHeading>
-        <a
-          className='lg:w-[250px] xs:w-[200px] w-[150px] mt-4 mb-10 xs:mb-20 self-center gap-8'
-          href='https://www.trinitybank.cz/'
-          target='_blank'
-        >
-          <img src={trinityBank} alt='Trinity Bank' />
-        </a>
-      </div>
-
-      <div className='self-center'>
-        <PartnerHeading>Hlavní partneři</PartnerHeading>
-      </div>
-      <div className='flex mb-14 xs:mb-20 flex-wrap justify-center gap-8 xs:gap-12'>
-        {mainPartners.map((item, index) => (
-          <a
-            key={index}
-            className='lg:w-[170px] xs:w-[150px] w-[120px] h-auto self-center'
-            href={item.linkURL}
-            target='_blank'
-          >
-            <img className='w-full' src={item.logoSrc} alt={item.name} />
-          </a>
-        ))}
-      </div>
-
-      <PartnerHeading>Logistický partner</PartnerHeading>
-      <a
-        className='flex flex-col lg:w-[200px] xs:w-[170px] w-[130px] mb-14 xs:mb-20 self-center gap-8'
-        href='https://www.postabezhranic.cz/'
-        target='_blank'
-      >
-        <img src={postaBezHranic} alt='Posta bez hranic' />
-      </a>
-
-      <PartnerHeading>Partner pro RoadShow Majáles UTB 2024</PartnerHeading>
-      <a
-        className='flex flex-col lg:w-[150px] xs:w-[150px] w-[100px] mb-14 xs:mb-20 self-center gap-8'
-        href='https://vestzlin.cz/'
-        target='_blank'
-      >
-        <img src={vest} alt='Vest' />
-      </a>
-
       <div className='self-center'>
         <PartnerHeading>Partneři</PartnerHeading>
       </div>
       <div className='flex flex-wrap justify-center align-middle gap-8 xs:gap-12'>
-        {partners.map((item, index) => (
-          <a
-            key={index}
-            className='lg:w-[150px] xs:w-[130px] w-[100px] h-auto self-center'
-            href={item.linkURL}
-            target='_blank'
-          >
-            <img className='w-full' src={item.logoSrc} alt={item.name} />
-          </a>
-        ))}
+        <h6>Chcete se stát partnery tohoto ročníku, tak přesne Vás potřebujeme. Kontaktujte nás na 
+            <a href="mailto:kratochvil@sutb.cz"> kratochvil@sutb.cz</a>
+        </h6>
       </div>
     </div>
   );
